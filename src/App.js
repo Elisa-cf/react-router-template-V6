d1ca842210afbc2a87c87270624571559381b79f
Elisa <elisacanyelles@gmail.com>
2021-11-06 17:24:51 +0100

    added routes and imported components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import Users from './components/Users';
+import Contact from './components/Contact';
+import Header from './components/Header';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <header>
+     <Header />
+     </header>
+     <Routes>
+       <Route exact path='/' element={<Home />}/>
+       <Route path='/users' element={<Users />}/>
+       <Route path='/contact' element={<Contact />}/>
+      {/* instead of /contact you could put /pizza, it doesn't matter. */}
+     </Routes>
     </div>
   );
 }
